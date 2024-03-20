fa7fe55a256949f95c81d7c9bc2065418d968043
PawelGozdz <pawelgozdz83@gmail.com>
2024-03-20 18:29:25 +0100

    refactor(helmet): additional security properties added (HI-80)
    CSP:
    defaultSrc: Allows content from same origin.
    scriptSrc: Disallows external scripts.
    styleSrc: Disallows external stylesheets.
    objectSrc: Disallows plugins.
    
    COEP:
    policy: 'require-corp': Ensures cross-origin isolation.
    
    CORP:
    policy: 'same-site': Limits cross-origin resource sharing.
    
    Hide Powered By:
    true: Hides server framework info.
    
    Frameguard:
    action: 'deny': Prevents framing.
    
    HSTS:
    maxAge: Enforces HSTS policy time.
    includeSubDomains: Applies to subdomains.
    preload: Adds to HSTS preload list.
    
    IE No Open:
    true: Blocks unsafe HTML files.
    
    Permitted Cross-Domain Policies:
    permittedPolicies: 'none': Disables cross-domain policies.
    
    Referrer Policy:
    policy: 'no-referrer': Enhances user privacy.
    
    XSS Filter:
    true: Activates browser XSS filter.

--- src/security-configuration.ts
+++ src/security-configuration.ts
@@ -3,7 +3,33 @@ import { json, urlencoded } from 'express';
 import helmet from 'helmet';
 
 export const nestApplicationSecirityConfiguration = (app: INestApplication) => {
-  app.use(helmet());
+  app.use(
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: ["'self'"],
+          scriptSrc: ["'none'"],
+          styleSrc: ["'none'"],
+          objectSrc: ["'none'"],
+        },
+      },
+      crossOriginEmbedderPolicy: { policy: 'require-corp' },
+      crossOriginResourcePolicy: { policy: 'same-site' },
+      hidePoweredBy: true,
+      frameguard: {
+        action: 'deny',
+      },
+      hsts: {
+        maxAge: 31536000,
+        includeSubDomains: true,
+        preload: true,
+      },
+      ieNoOpen: true,
+      permittedCrossDomainPolicies: { permittedPolicies: 'none' },
+      referrerPolicy: { policy: 'no-referrer' },
+      xssFilter: true,
+    }),
+  );
 
   app.enableCors();
 
