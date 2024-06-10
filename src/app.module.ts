6f2f332290f839539192b014dd171e07f3509cf2
PawelGozdz <pawelgozdz83@gmail.com>
2024-06-10 22:18:47 +0200

    refactor(outbox): outbox cron funrctionality (HI-97)

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { LoggerModule } from 'nestjs-pino';
 
 import { ApiGatewayModule } from '@app/api-gateway';
 import config from '@app/config';
-import { Database, DatabaseModule, GracefulShutdownService, OpenTelemetryModuleModule, OutboxModule } from '@app/core';
+import { Database, DatabaseModule, GracefulShutdownService, OpenTelemetryModuleModule } from '@app/core';
 import { AppUtils } from '@libs/common';
 import { CqrsModule } from '@libs/cqrs';
 
@@ -36,7 +36,6 @@ const providers = [GracefulShutdownService];
     CqrsModule,
     LoggerModule.forRoot(config.pinoLogger),
     OpenTelemetryModuleModule,
-    OutboxModule,
   ],
   providers: [...providers],
 })
